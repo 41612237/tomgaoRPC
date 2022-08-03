5f3f6b18940936f067a3fcc5525870134e7feeeb
gaoliyun <376801941@qq.com>
2022-08-03 22:51:44 +0900

    [v2.1] 增加了Netty客户端连接失败重试机制, 改变Socket方式传输数据的端序, 使得能与Netty通信

--- rpc-core/src/main/java/com/tomgao/rpc/netty/client/NettyClient.java
+++ rpc-core/src/main/java/com/tomgao/rpc/netty/client/NettyClient.java
@@ -18,6 +18,9 @@ import io.netty.util.AttributeKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.InetSocketAddress;
+import java.util.concurrent.atomic.AtomicReference;
+
 public class NettyClient implements RpcClient {
 
     private static final Logger logger = LoggerFactory.getLogger(NettyClient.class);
@@ -50,26 +53,16 @@ public class NettyClient implements RpcClient {
             logger.error("未设置序列化器");
             throw new RpcException(RpcError.UNKNOWN_SERIALIZER);
         }
-        bootstrap.handler(new ChannelInitializer<SocketChannel>() {
-            @Override
-            protected void initChannel(SocketChannel socketChannel) throws Exception {
-                ChannelPipeline pipeline = socketChannel.pipeline();
-                pipeline.addLast(new CommonDecoder())
-                        .addLast(new CommonEncoder(serializer))
-                        .addLast(new NettyClientHandler());
-            }
-        });
+        AtomicReference<Object> result = new AtomicReference<>(null);
 
         try {
-            ChannelFuture future = bootstrap.connect(host, port).sync();
-            logger.info("客户端连接到服务器 {}: {}", host, port);
-            Channel channel = future.channel();
-            if (channel != null) {
-                channel.writeAndFlush(rpcRequest).addListener(future1 -> {
-                    if (future1.isSuccess()) {
-                        logger.info("客户端发送消息: {}", rpcRequest.toString());
+            Channel channel = ChannelProvider.get(new InetSocketAddress(host, port), serializer);
+            if (channel.isActive()) {
+                channel.writeAndFlush(rpcRequest).addListener(future -> {
+                    if (future.isSuccess()) {
+                        logger.info("客户端发送消息:{}", rpcRequest.toString());
                     } else {
-                        logger.error("发送消息时有错误发生", future1.cause());
+                        logger.error("发送消息时有错误发生: ", future.cause());
                     }
                 });
                 channel.closeFuture().sync();
@@ -77,12 +70,14 @@ public class NettyClient implements RpcClient {
                 AttributeKey<RpcResponse> key = AttributeKey.valueOf("rpcResponse" + rpcRequest.getRequestId());
                 RpcResponse rpcResponse = channel.attr(key).get();
                 RpcMessageChecker.check(rpcRequest, rpcResponse);
-                return rpcResponse.getData();
+                result.set(rpcResponse.getData());
+            } else {
+                System.exit(0);
             }
         } catch (InterruptedException e) {
             logger.error("发送消息时有错误发生", e);
         }
-        return null;
+        return result.get();
     }
 
     public void setSerializer(CommonSerializer serializer) {
