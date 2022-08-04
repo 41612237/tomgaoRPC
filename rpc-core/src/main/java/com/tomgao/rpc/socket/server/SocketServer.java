52e29b6ab7028c4878a4c6a935a0e16082be3de7
gaoliyun <376801941@qq.com>
2022-08-04 11:51:30 +0900

    [v2.2] 修整代码, 使用ThreadPoolFactory创建Socket版本的线程池

--- rpc-core/src/main/java/com/tomgao/rpc/socket/server/SocketServer.java
+++ rpc-core/src/main/java/com/tomgao/rpc/socket/server/SocketServer.java
@@ -6,6 +6,7 @@ import com.tomgao.rpc.enumeration.RpcError;
 import com.tomgao.rpc.exception.RpcException;
 import com.tomgao.rpc.registry.ServiceRegistry;
 import com.tomgao.rpc.serializer.CommonSerializer;
+import com.tomgao.rpc.util.ThreadPoolFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,10 +18,6 @@ import java.util.concurrent.*;
 public class SocketServer implements RpcServer {
 
     private static final Logger logger = LoggerFactory.getLogger(SocketServer.class);
-    private static final int CORE_POOL_SIZE = 5;
-    private static final int MAXIMUM_POOL_SIZE = 50;
-    private static final int KEEP_ALIVE_TIME = 60;
-    private static final int BLOCKING_QUEUE_CAPACITY = 100;
 
     private CommonSerializer serializer;
 
@@ -32,9 +29,10 @@ public class SocketServer implements RpcServer {
 
     public SocketServer(ServiceRegistry serviceRegistry) {
         this.serviceRegistry = serviceRegistry;
-        BlockingQueue<Runnable> workingQueue = new ArrayBlockingQueue<>(BLOCKING_QUEUE_CAPACITY);
-        ThreadFactory threadFactory = Executors.defaultThreadFactory();
-        threadPool = new ThreadPoolExecutor(CORE_POOL_SIZE, MAXIMUM_POOL_SIZE, KEEP_ALIVE_TIME, TimeUnit.SECONDS, workingQueue, threadFactory);
+        threadPool = ThreadPoolFactory.createDefaultThreadPool("socket-rpc-server");
+//        BlockingQueue<Runnable> workingQueue = new ArrayBlockingQueue<>(BLOCKING_QUEUE_CAPACITY);
+//        ThreadFactory threadFactory = Executors.defaultThreadFactory();
+//        threadPool = new ThreadPoolExecutor(CORE_POOL_SIZE, MAXIMUM_POOL_SIZE, KEEP_ALIVE_TIME, TimeUnit.SECONDS, workingQueue, threadFactory);
     }
 
     @Override
